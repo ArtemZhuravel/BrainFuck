f4d4bc3131025ad3544f67059f6f4c4089fe9403
ArtemZhuravell <Zhuravelll.Artem@gmail.com>
2018-08-20 00:21:49 +0300

    New Commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Choose one of the following languages:\n1.NoCommand;\n2.BrainFuck\n");
+        Scanner scanner = new Scanner(System.in);
+        scanner.next();
+        String code = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
+
+    }
+}
